dc8e265329bdc271da5a8cdf86f09ccd753055c6
Tinnoha <tspitsyn@sfedu.ru>
2025-04-20 16:24:27 +0300

    Delete models directory

--- models/models.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type User struct {
-	FullName     string `json:"full_name"`
-	Username     string `json:"username"`
-	Email        string `json:"email"`
-	PasswordHash string `json:"password_hash"`
-	Role         string `json:"role"`
-}
