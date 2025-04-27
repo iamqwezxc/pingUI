9668ae27aeec6e6b51174b626c0122c71d838db6
Tinnoha <tspitsyn@sfedu.ru>
2025-04-27 14:03:34 +0300

    Create jsons.go

---
+++ Backend/pkg/json_jwt/jsons.go
@@ -0,0 +1,68 @@
+package jsons
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	model "studyproject/models"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var secretKey = []byte("my_secret_key")
+
+func JSONtoStruct(c *gin.Context) (model.User, error) {
+	var user model.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return model.User{}, err
+	}
+	return user, nil
+}
+
+func ParseToken(tokenString string) (*jwt.Token, error) {
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return secretKey, nil
+	})
+}
+
+func GenerateToken(userID int) (string, error) {
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(time.Hour * 3).Unix(), // Срок действия — 3 часа
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(secretKey)
+}
+
+func AuthMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(w, "Токен не предоставлен", http.StatusUnauthorized)
+			return
+		}
+
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+		token, err := ParseToken(tokenString)
+		if err != nil || !token.Valid {
+			http.Error(w, "Неверный токен", http.StatusUnauthorized)
+			return
+		}
+
+		next(w, r)
+	}
+}
+
+func ProtectedHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Вы получили доступ!")
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(bytes), err
+}
