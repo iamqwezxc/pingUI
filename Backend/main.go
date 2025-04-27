f7992c6beff2a636faa7e40956a73d917bb4d3e2
Tinnoha <tspitsyn@sfedu.ru>
2025-04-27 14:01:17 +0300

    Add files via upload

--- Backend/main.go
+++ Backend/main.go
@@ -1,28 +1,21 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"project/database"
-	"project/handlers"
-
-	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
+
+	model "studyproject/models"
+	database "studyproject/pkg/db_database"
+
+	//JWTs "studyproject/pkg/json_jwt"
+	website "studyproject/pkg/wb_website"
 )
 
 func main() {
-	db, err := database.Connect("user=postgres password=psql dbname=postgres sslmode=disable")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
 
-	router := mux.NewRouter()
+	//http.HandleFunc("/user", JWTs.AuthMiddleware(JWTs.ProtectedHandler))
+	//http.ListenAndServe(":8080", nil)
 
-	router.HandleFunc("/api/users", handlers.CreateUserHandler(db)).Methods("POST")
-
-	fmt.Println("Сервер запущен на порту 8280")
-	log.Fatal(http.ListenAndServe(":8280", router))
+	database.DBConnect(model.ConnStrUsers)
+	website.WBStarsWebSite()
 
 }
