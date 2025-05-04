28447c3f7e94f483d60bda170f36716a7bd94510
Tinnoha <tspitsyn@sfedu.ru>
2025-05-04 18:37:14 +0300

    Сommit

--- Backend/pkg/wb_website/websites.go
+++ Backend/pkg/wb_website/websites.go
@@ -1,19 +1,46 @@
 package websites
 
 import (
+	"log"
 	"net/http"
 
+	"github.com/gin-gonic/gin"
+	model "github.com/iamqwezxc/pingUI/Backend/models"
 	database "github.com/iamqwezxc/pingUI/Backend/pkg/db_database"
 	JSONJWT "github.com/iamqwezxc/pingUI/Backend/pkg/json_jwt"
-
-	"github.com/gin-gonic/gin"
 )
 
 func WBStarsWebSite() {
 	r := gin.Default()
 
 	r.GET("/regist", func(c *gin.Context) {
-		c.String(http.StatusOK, "Главная страница")
+		db := database.DBConnect(model.ConnStrUsers)
+		rows, err := db.Query("SELECT user_id, full_name, Username, Email, Password_Hash, Role FROM users")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+
+		for rows.Next() {
+			var user_id int
+			var full_name string
+			var Username string
+			var Email string
+			var Password_Hash string
+			var Role string
+			if err := rows.Scan(&user_id, &full_name, &Username, &Email, &Password_Hash, &Role); err != nil {
+				log.Fatal(err)
+			}
+			c.String(http.StatusOK, full_name+" ")
+			c.String(http.StatusOK, Username+" ")
+			c.String(http.StatusOK, Email+" ")
+			c.String(http.StatusOK, Password_Hash+" ")
+			c.String(http.StatusOK, Role+" ")
+			c.String(http.StatusOK, "\n")
+
+		}
+		defer db.Close()
+
 	})
 
 	r.POST("/regist", func(c *gin.Context) {
